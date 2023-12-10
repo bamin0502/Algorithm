c99c7330042e87f426ca783cf90ff2fbd2b90421
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-12-10 18:05:09 +0900

    프로그래머스도 테스트

---
+++ Programmers/프로그래머스/common multiple.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int number, int n , int m){
+    return (number % n == 0 && number % m == 0) ? 1:0;
+}
+int main() {
+    int number, n, m;
+    cout << "세 정수를 입력하세요 (number, n, m): ";
+    cin >> number >> n >> m;
+    
+    cout << "결과: " << solution(number, n, m) << endl;
+    return 0;
+}
