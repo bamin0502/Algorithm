86efa1d087f465a750974ff169ebd6c6010559e5
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-12-14 21:07:30 +0900

    알고리즘 추가

---
+++ Baekjoon/백준/2525.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    B += C;
+    A += B / 60;
+    B = B % 60;
+    A = A % 24;
+
+    cout << A << " " << B;
+
+    return 0;
+}
---
+++ Baekjoon/백준/2884.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int H;
+    int M;
+
+    cin>>H>>M;
+
+    if(M < 45){
+        M = M + 15;
+        H--;
+        if(H < 0){
+            H = 23;
+        }
+    }
+    else{
+        M = M - 45;
+    }
+
+    cout << H << " " << M;
+
+    return 0;
+    
+}
+
