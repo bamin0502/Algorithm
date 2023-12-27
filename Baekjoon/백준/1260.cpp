4d39138e4fbbc330a4e5a7174862ebab39453337
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-12-28 04:22:39 +0900

    알고리즘 문제 추가

---
+++ Baekjoon/백준/1260.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int N, M, V;
+vector<int> adj[1001];
+bool visited[1001];
+
+void dfs(int node) {
+    cout << node << ' ';
+    visited[node] = true;
+    for (int i = 0; i < adj[node].size(); i++) {
+        int next = adj[node][i];
+        if (!visited[next]) dfs(next);
+    }
+}
+
+void bfs(int start) {
+    queue<int> q;
+    fill_n(visited, 1001, false);
+    q.push(start);
+    visited[start] = true;
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << ' ';
+        for (int i = 0; i < adj[node].size(); i++) {
+            int next = adj[node][i];
+            if (!visited[next]) {
+                q.push(next);
+                visited[next] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M >> V;
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for (int i = 1; i <= N; i++) {
+        sort(adj[i].begin(), adj[i].end());
+    }
+    dfs(V);
+    cout << '\n';
+    bfs(V);
+    cout << '\n';
+    return 0;
+}
+
---
+++ Baekjoon/백준/2178.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <queue>
+#define MAX 100
+
+using namespace std;
+
+int N, M;
+int map[MAX][MAX];
+bool visit[MAX][MAX];
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+void BFS(int x, int y) {
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    visit[x][y] = true;
+
+    while (!q.empty()) {
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                if (map[nx][ny] == 1 && !visit[nx][ny]) {
+                    map[nx][ny] = map[x][y] + 1;
+                    visit[nx][ny] = true;
+                    q.push(make_pair(nx, ny));
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%1d", &map[i][j]);
+        }
+    }
+
+    BFS(0, 0);
+    cout << map[N - 1][M - 1] << '\n';
+
+    return 0;
+}
+
